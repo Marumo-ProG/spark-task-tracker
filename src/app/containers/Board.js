262915fdbc49ff7396d1bed5996a1d015f5bda1e
Lenny <lenny@port443.co.za>
2025-07-01 13:38:49 +0200

    menus

--- src/app/containers/Board.js
+++ src/app/containers/Board.js
@@ -1,8 +1,13 @@
+"use client";
+import { useState, useEffect } from "react";
+
 // MUI
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import Chip from "@mui/material/Chip";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 // Containers
 import Task from "./Task";
@@ -11,30 +16,85 @@ import Task from "./Task";
 import { Colors } from "@/common/constants";
 
 // Icons
-import AddRoundedIcon from '@mui/icons-material/AddRounded';
-import MoreHorizRoundedIcon from '@mui/icons-material/MoreHorizRounded';
+import AddRoundedIcon from "@mui/icons-material/AddRounded";
+import MoreHorizRoundedIcon from "@mui/icons-material/MoreHorizRounded";
 
-const Board = ({title,taskList=[] }) => {
-    return (
-        <Stack padding={2} sx={{backgroundColor: Colors.lightGrey, borderRadius: "8px", minHeight: "400px"}}>
-            <Stack direction="row" alignItems={"center"} justifyContent="space-between" maxHeight="56px">
-                <Typography variant="h6" sx={{fontSize: 15}}>{title}</Typography>
-                <Stack direction={"row"} spacing={1} alignItems="center">
-                    <IconButton sx={{ backgroundColor: Colors.medGrey, color: Colors.black }} aria-label="add task">
-                        <AddRoundedIcon sx={{fontSize: 15}}/>
-                    </IconButton>
-                    <IconButton sx={{ backgroundColor: Colors.medGrey, color: Colors.black }} aria-label="menu">
-                        <MoreHorizRoundedIcon sx={{fontSize: 15}}/>
-                    </IconButton>
-                </Stack>
-            </Stack>
-            <Stack spacing={2} mt={2}>
-                {taskList.map((task, index) => (
-                    <Task key={index} task={task} />
-                ))}
-            </Stack>
+const Board = ({ title, taskList = [] }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  return (
+    <>
+      <Stack
+        padding={2}
+        sx={{
+          backgroundColor: Colors.lightGrey,
+          borderRadius: "8px",
+          minHeight: "400px",
+        }}
+      >
+        <Stack
+          direction="row"
+          alignItems={"center"}
+          justifyContent="space-between"
+          maxHeight="56px"
+        >
+          <Typography variant="h6" sx={{ fontSize: 15 }}>
+            {title}
+          </Typography>
+          <Stack direction={"row"} spacing={1} alignItems="center">
+            <IconButton
+              sx={{ backgroundColor: Colors.medGrey, color: Colors.black }}
+              aria-label="add task"
+            >
+              <AddRoundedIcon sx={{ fontSize: 15 }} />
+            </IconButton>
+            <IconButton
+              onClick={(e) => setAnchorEl(e.currentTarget)}
+              sx={{ backgroundColor: Colors.medGrey, color: Colors.black }}
+              aria-label="menu"
+            >
+              <MoreHorizRoundedIcon sx={{ fontSize: 15 }} />
+            </IconButton>
+          </Stack>
         </Stack>
-    )
-}
+        <Stack spacing={2} mt={2}>
+          {taskList.map((task, index) => (
+            <Task key={index} task={task} />
+          ))}
+        </Stack>
+      </Stack>
+      <Menu open={open} anchorEl={anchorEl} onClose={() => setAnchorEl(null)}>
+        <MenuItem
+          onClick={() => {
+            setAnchorEl(null);
+          }}
+        >
+          Edit Board
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            setAnchorEl(null);
+          }}
+        >
+          Delete Board
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            setAnchorEl(null);
+          }}
+        >
+          Remove Overdue Tasks
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            setAnchorEl(null);
+          }}
+        >
+          Clear
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
 
 export default Board;
--- src/app/containers/Task.js
+++ src/app/containers/Task.js
@@ -1,3 +1,10 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+// Date fns
+import { format, parseISO } from "date-fns";
+
 // MUI
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
@@ -9,80 +16,139 @@ import Chip from "@mui/material/Chip";
 import Divider from "@mui/material/Divider";
 import Avatar from "@mui/material/Avatar";
 import AvatarGroup from "@mui/material/AvatarGroup";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 // Constants
 import { Colors } from "@/common/constants";
 
 // Icons
 import AttachFileRoundedIcon from "@mui/icons-material/AttachFileRounded";
-import FlagRoundedIcon from '@mui/icons-material/FlagRounded';
-import AccessTimeFilledRoundedIcon from '@mui/icons-material/AccessTimeFilledRounded';
+import FlagRoundedIcon from "@mui/icons-material/FlagRounded";
+import AccessTimeFilledRoundedIcon from "@mui/icons-material/AccessTimeFilledRounded";
+import MoreHorizRoundedIcon from '@mui/icons-material/MoreHorizRounded';
 
 const Task = ({ task, onEdit, onDelete }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const parsedDate = parseISO(task.due_date);
+  const formated_date = format(parsedDate, "MMM dd");
   return (
-    <Paper elevation={0} sx={{}}>
-      <Stack padding={2} spacing={2}>
-        <Typography variant="h6" sx={{ fontSize: 14, fontWeight: 700 }}>
-          {task.title}
-        </Typography>
-        <Typography
-          variant="body2"
-          sx={{ fontSize: 12, color: Colors.medGrey }}
-        >
-          {task.description}
-        </Typography>
-        <Chip
-          label={task.author.name}
-          size="small"
-          sx={{
-            backgroundColor: Colors.primary,
-            color: Colors.white,
-            borderRadius: "8px",
-            width: "fit-content",
-          }}
-        />
-      </Stack>
-      <Divider />
-      <Stack
-        height="46px"
-        direction={"row"}
-        alignItems={"center"}
-        justifyContent="space-between"
-        px={2}
-      >
-        <Stack direction="row" spacing={1} alignItems="center">
-          
-          <Stack direction="row" spacing={0.5} alignItems="center" sx={{ cursor: "pointer" }}>
-            <AttachFileRoundedIcon sx={{ fontSize: 20, color: Colors.primary }} />
-            <Typography variant="body2" sx={{ fontSize: 12, color: Colors.medGrey }}>
-              1
-            </Typography>
-        </Stack>
+    <>
+      <Paper elevation={0} sx={{}}>
+        <Stack padding={2} spacing={2}>
+          <Stack direction="row" alignItems={"center"} justifyContent="space-between">
+          <Typography variant="h6" sx={{ fontSize: 14, fontWeight: 700 }}>
+            {task.title}
+          </Typography>
           <IconButton
-            sx={{ padding: 0.5}}
-            aria-label="flag task"
-            >
-            <FlagRoundedIcon sx={{ fontSize: 20, color: "red" }} />
+            sx={{ backgroundColor: Colors.medGrey, color: Colors.black }}
+            aria-label="more options"
+            onClick={(e) => setAnchorEl(e.currentTarget)}
+          >
+            <MoreHorizRoundedIcon sx={{ fontSize: 15 }} />
           </IconButton>
-          <IconButton
-         
-            aria-label="due date"
-            >
-            <AccessTimeFilledRoundedIcon sx={{ fontSize: 20, color: "green" }} />
-            </IconButton>
+          </Stack>
+          <Typography
+            variant="body2"
+            sx={{ fontSize: 12, color: Colors.medGrey }}
+          >
+            {task.description}
+          </Typography>
+          <Chip
+            label={task.author.name}
+            size="small"
+            sx={{
+              backgroundColor: Colors.primary,
+              color: Colors.white,
+              borderRadius: "8px",
+              width: "fit-content",
+            }}
+          />
         </Stack>
-        <AvatarGroup max={2} sx={{ "& .MuiAvatar-root": { width: 24, height: 24 } }}>
-            {task.assigned_to.map((assignee, index) => (
-                <Avatar
-                    key={index}
-                    alt={assignee.name}
-                    src={assignee.image.src}
-                    sx={{ width: 24, height: 24, cursor: "pointer" }}
+        <Divider />
+        <Stack
+          height="46px"
+          direction={"row"}
+          alignItems={"center"}
+          justifyContent="space-between"
+          px={2}
+        >
+          <Stack direction="row" spacing={1} alignItems="center">
+            <Stack
+              direction="row"
+              spacing={0.5}
+              alignItems="center"
+              sx={{ cursor: "pointer" }}
+            >
+              <AttachFileRoundedIcon
+                sx={{ fontSize: 20, color: Colors.primary }}
+              />
+              <Typography
+                variant="body2"
+                sx={{ fontSize: 12, color: Colors.medGrey }}
+              >
+                1
+              </Typography>
+            </Stack>
+            <IconButton sx={{ padding: 0.5 }} aria-label="flag task">
+              <FlagRoundedIcon sx={{ fontSize: 20, color: "red" }} />
+            </IconButton>
+
+            <Stack direction="row" spacing={1} alignItems="center">
+              <Stack
+                direction="row"
+                spacing={0.5}
+                alignItems="center"
+                sx={{ cursor: "pointer" }}
+              >
+                <AccessTimeFilledRoundedIcon
+                  sx={{ fontSize: 20, color: Colors.primary }}
                 />
-                ))}
-        </AvatarGroup>
-      </Stack>
-    </Paper>
+                <Typography
+                  variant="body2"
+                  sx={{ fontSize: 12, color: Colors.black }}
+                >
+                  {formated_date}
+                </Typography>
+              </Stack>
+            </Stack>
+          </Stack>
+          <AvatarGroup
+            max={2}
+            sx={{ "& .MuiAvatar-root": { width: 24, height: 24 } }}
+          >
+            {task.assigned_to.map((assignee, index) => (
+              <Avatar
+                key={index}
+                alt={assignee.name}
+                src={assignee.image.src}
+                sx={{ width: 24, height: 24, cursor: "pointer" }}
+              />
+            ))}
+          </AvatarGroup>
+        </Stack>
+      </Paper>
+      <Menu open={open} anchorEl={anchorEl} onClose={() => setAnchorEl(null)}>
+        <MenuItem
+          onClick={() => {
+            onEdit(task);
+            setAnchorEl(null);
+          }}
+        >
+          Edit Task
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            onDelete(task.id);
+            setAnchorEl(null);  
+          }}
+        >
+          Delete Task
+        </MenuItem>
+      </Menu>
+    </>
   );
 };
 
